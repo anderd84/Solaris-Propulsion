c7f9a7500b218db013e4fd0e93a47f6e97bb0242
Garth <nandakg@my.erau.edu>
2024-04-05 00:42:28 -0700

    Incorporated pyromat. Now I can put the dewar pressure in and it auto updates the LOX density!!! took way longer than i thought to figure out the kinks of pyromat

--- Doublet.py
+++ Doublet.py
@@ -5,7 +5,7 @@ from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 from pint import UnitRegistry
 import numpy as np
-
+import pyromat as pm
 
 """
 Shit that needs to get done still in this code:
@@ -17,11 +17,18 @@ Shit that needs to get done still in this code:
     Make all code blocks into functions - EASIER TO READ. So david no kill me
 """
 
-
+pm.config['unit_pressure'] = 'psi'
+pm.config['unit_mass'] = 'lbm'
+pm.config['unit_matter'] = 'lbm'
+pm.config['unit_length'] = 'ft'
+pm.config['unit_volume'] = 'ft3'
 ureg = UnitRegistry()
 ureg.default_system = 'US'
 Q_ = ureg.Quantity
 
+N2 = pm.get('mp.N2')
+LOX = pm.get('mp.O2')
+CO2 = pm.get('mp.CO2')
 
 class PROP:
     def __init__(self, gamma, mdot, rho):
@@ -79,6 +86,7 @@ class PROP:
 #Constants
 CD_drill = 0.7 #Constant for Sharp Edged Orifices
 g0 = Q_(32.174, ureg.foot / ureg.second**2)
+Prescott_pressure = Q_(12.04, ureg.force_pound / ureg.inch**2)
 #Design Input
 mdots = np.array([5.29, 2.21])/4 #LOX_CORE, FUEL_CORE
 Film_Cooling = np.array([0.08, 0.08]) #Outer Film Cooling, Inner Film Cooling
@@ -89,8 +97,14 @@ Rgamma_lox = 1.55  #Radial distance between centerline and LOX hole
 Pressure_Drop_Fuel = 0.2 #Pressure drop Percentage (ROT: Always in terms of Chamber Pressure)
 Pressure_Drop_Lox = 0.2 #Pressure drop Percentage (ROT: Always in terms of Chamber Pressure)
 Pressure_Chamber = Q_(300, ureg.force_pound / ureg.inch**2) #Chamber Pressure Pretty Obvious
-Doublet_Diameter_LOX = Q_(0.125, ureg.inch)  #Design choise for DOublet Diameter size (Need to look more properly into it as 1/4 holes might make vaporization time too long)
-OX_CORE = PROP(gamma=20., mdot=mdots[0], rho=56.794)
+Doublet_Diameter_LOX = Q_(0.125, ureg.inch)  #Design choise for DOublet Diameter size (Need to look more properly into it as 1/4 holes might make vaporization time too long)\
+Lox_Dewar_Pressure = Q_(230, ureg.force_pound / ureg.inch**2) + Prescott_pressure
+LOX_Sat_Temp = LOX.Ts(p=Lox_Dewar_Pressure.magnitude )
+LOX_Sat_Dens = LOX.ds(T=LOX_Sat_Temp)
+
+
+OX_CORE = PROP(gamma=20., mdot=mdots[0], rho=LOX_Sat_Dens[0][0])
+print(OX_CORE.rho)
 FUEL_CORE = PROP(gamma = 0, mdot = mdots[1], rho=51.15666) #gamma zero for this one because it's the initialized guess just making the FUEL CORE class requires it ( should change when moving to data classes)
 OUT_FILM_C = PROP(gamma = 30, mdot = Film_Cooling[0]* FUEL_CORE.mdot, rho = FUEL_CORE.rho)
 IN_FILM_C = PROP(gamma = -30, mdot = Film_Cooling[1]* FUEL_CORE.mdot, rho = FUEL_CORE.rho)
