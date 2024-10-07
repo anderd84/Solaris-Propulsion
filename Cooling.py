0b28e0a6794b09dd5dde34866a596be01433b6be
Dylan Nguyen <DylanNguyen888@gmail.com>
2024-10-07 11:47:05 -0700

    Added formulas for calculating the convection coefficient for the cooling channels using the Gnielinski and Sieder-Tate correlations

--- Cooling.py
+++ Cooling.py
@@ -23,3 +23,24 @@ sigma = 1 / ((1 / 2 * T_wg / T_0g * (1 + (gamma - 1) / 2 * Ma**2) + 1 / 2)**(0.8
 h_g = 0.026 / D_star**0.2 * mu**0.2 / Pr**0.6 * c_p * (P_0 / c_star)**0.8 * (D_star / r_c)**0.1 * (A_star / A)**0.9 * sigma  # Convective heat transfer coefficient
 
 print(f"Combustion side convective heat transfer coefficient: {h_g:.2f} W/m^2/K")
+
+# Gniliesnski/Sieder & Tate for channel side
+# Inputs (Cooling channel)
+f = 0.000   # Friction factor
+rho = 1000  # Coolant density
+v = 10      # Coolant velocity along channel
+D_h = 4     # Hydraulic diameter of cooling channel
+Pr = 1      # Prandtl number (likely an array)
+mu = 0.01   # Dynamic viscosity (likely an array)
+k_c = 20    # Thermal conductivity of coolant
+mu_s = 1    # Dynamic viscosity at the heat tranfer boundary surface temperature
+
+# Calculations
+Re_D = rho*v*D_h/mu     # Reynold's number
+# Re_D = 4*m_dot_c/numpy.pi/D_h/mu      # Alternate formula
+if Pr >= 0.7 and Pr <= 16700 and Re_D >= 3000 and Re_D <= 5000000:   # Check that properties fit restrictions for Gnielinski
+    Nu_D = f/8*(Re_D - 1000)*Pr / (1 + 12.7*(f/8)**0.5 * (Pr**(2/3) - 1))   # Nusselt number of fully developed flow
+    h_c = Nu_D*k_c/D_h      # Convective heat transfer coefficient
+else:    # Use Sieder & Tate otherwise
+    Nu_D = 0.027*Re_D**0.8*Pr**(1/3)*(mu/mu_s)**0.14 # Nusselt number of fully developed flow
+    h_c = Nu_D*k_c/D_h      # Convective heat transfer coefficient
