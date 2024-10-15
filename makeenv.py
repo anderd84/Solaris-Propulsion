42bb77c4450d860bedd0cb6a1431facd356c0d4e
anderd84 <anderd84@my.erau.edu>
2024-10-14 19:57:52 -0700

    made it even more versitile

--- makeenv.py
+++ makeenv.py
@@ -2,8 +2,8 @@ import sys
 import subprocess
 import os
 
-cwd = os.getcwd()
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 print("installing stuff :)")
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--editable", cwd])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--editable", dir_path])
 print("done!")
