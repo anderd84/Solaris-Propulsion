ecbc3cc7c6c5c49741f561270d5272caa63a2f09
David Anderson <anderd84@my.erau.edu>
2024-10-26 14:50:48 -0700

    stashed

--- src/Cooling/domain.py
+++ src/Cooling/domain.py
@@ -3,6 +3,7 @@ from types import NoneType
 import numpy as np
 
 from Cooling.material import DomainMaterial
+from Cooling import material
 
 @dataclass
 class DomainPoint:
@@ -36,7 +37,26 @@ class Domain:
         self.rstep = height/vpoints
         for i in range(hpoints):
             for j in range(vpoints):
-                self.array[i][j] = DomainPoint(x0 + i*self.xstep, r0 + j*self.rstep, DomainMaterial.FREE, 0, self.xstep*self.rstep)
+                self.array[i][j] = DomainPoint(x0 + i*self.xstep, r0 - j*self.rstep, DomainMaterial.FREE, 0, self.xstep*self.rstep)
+
+    def DefineMaterials(self, cowl: np.ndarray, coolant: np.ndarray, chamber: np.ndarray, plug: np.ndarray):
+        for i in range(self.hpoints):
+            for j in range(self.vpoints):
+                if material.isIntersect(self.array[i][j], cowl, (self.width, self.height)):
+                    self.array[i][j].material = DomainMaterial.COWL
+                if material.isIntersect(self.array[i][j], coolant, (self.width, self.height)):
+                    self.array[i][j].material = DomainMaterial.COOLANT
+                if material.isIntersect(self.array[i][j], chamber, (self.width, self.height)):
+                    self.array[i][j].material = DomainMaterial.CHAMBER
+                if material.isIntersect(self.array[i][j], plug, (self.width, self.height)):
+                    self.array[i][j].material = DomainMaterial.PLUG
+                
+                
+
+
 
 def SolveDomain(domain: Domain, tol: float = 1e-3):
-    
+    for i in range(domain.hpoints):
+        for j in range(domain.vpoints):
+            if i == 0 or j == 0 or i == domain.hpoints-1 or j == domain.vpoints-1:
+                print("edge")
--- src/Cooling/material.py
+++ src/Cooling/material.py
@@ -9,29 +9,32 @@ class DomainMaterial(Enum):
     FREE = 0 
     COWL = 1
     COOLANT = 2
-    CHAMBER = 3
+    contour = 3
     PLUG = 4
 
-def Intersect(Point, Chamber, MatrixSize):
+def isIntersect(Point, contour: np.ndarray[ContourPoint], domainSize: tuple[int, int]):
+    if contour.size == 0:
+        return False
     OnTheLine = 0
-    A = Point
-    B = [Point[0], Point[1]+ MatrixSize[1]]   #Making an "infinitely" long vertical line at the point
+    A = [Point.x, Point.r]   #Point
+    B = [Point.x, Point.r + domainSize[1]]   #Making an "infinitely" long vertical line at the point
     Check = 0
-    for i in range(len(Chamber) - 1):
-        C = [Chamber[i].x,Chamber[i].r]
-        D = [Chamber[i+1].x,Chamber[i+1].r]
+    for i in range(len(contour) - 1):
+        C = [contour[i].x,contour[i].r]
+        D = [contour[i+1].x,contour[i+1].r]
         Check = Check + (ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D))
         if is_point_on_segment(Point, C, D):
             OnTheLine = 1
             break
-    C = [Chamber[-1].x,Chamber[-1].r]
-    D = [Chamber[0].x,Chamber[0].r]
+    C = [contour[-1].x,contour[-1].r]
+    D = [contour[0].x,contour[0].r]
     Check = Check + (ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D))    
 
     InsideOutside = np.mod(Check,2) == 1 or OnTheLine == 1
-    return Check, OnTheLine, InsideOutside #Return true if point is inside the polygon
+    return InsideOutside #Return true if point is inside the polygon
 
 def is_point_on_segment(Point, C, D):
+    Point = [Point.x, Point.r]
     # Check collinearity using cross product
     collinear = (D[1] - C[1]) * (Point[0] - C[0]) == (Point[1] - C[1]) * (D[0] - C[0])
     # Check if point P is within the bounding box of C and D
@@ -44,14 +47,14 @@ def ccw(A,B,C):
 def main():
     Points = 1000
 
-    Chamber_x = np.concatenate([np.linspace(0, 5, Points),np.linspace(5, 5, Points),np.linspace(5, 0, Points),np.linspace(0, 0, Points)])
-    Chamber_r = np.concatenate([np.linspace(0, 0, Points),np.linspace(0, 5, Points),np.linspace(5, 5, Points),np.linspace(5, 0, Points)])
+    contour_x = np.concatenate([np.linspace(0, 5, Points),np.linspace(5, 5, Points),np.linspace(5, 0, Points),np.linspace(0, 0, Points)])
+    contour_r = np.concatenate([np.linspace(0, 0, Points),np.linspace(0, 5, Points),np.linspace(5, 5, Points),np.linspace(5, 0, Points)])
 
-    Chamber = [ContourPoint(row[0],row[1]) for row in np.transpose([Chamber_x,Chamber_r])]
-    Point = (2.0,-5.1)
+    contour = [ContourPoint(row[0],row[1]) for row in np.transpose([contour_x,contour_r])]
+    Point = ContourPoint(2.5,2.5)
 
-    Check, OnTheLine, InsideOutside  = Intersect(Point, Chamber, np.array([10000,10000]))
-    ic(Check, OnTheLine, InsideOutside)
+    Check = isIntersect(Point, contour, np.array([10000,10000]))
+    ic(Check)
 
 
 
