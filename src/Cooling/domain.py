2260ea0b4c0a320a8a28df594081c6054bbd9099
anderd84 <anderd84@my.erau.edu>
2024-10-27 11:21:10 -0700

    live domain plotting

--- src/Cooling/domain.py
+++ src/Cooling/domain.py
@@ -40,21 +40,33 @@ class Domain:
             for j in range(vpoints):
                 self.array[i][j] = DomainPoint(x0 + i*self.xstep, r0 - j*self.rstep, DomainMaterial.FREE, 0, self.xstep*self.rstep)
 
-    def DefineMaterials(self, cowl: np.ndarray, coolant: np.ndarray, chamber: np.ndarray, plug: np.ndarray):
+    def DefineMaterials(self, cowl: np.ndarray, coolant: np.ndarray, chamber: np.ndarray, plug: np.ndarray, fig: plt.Figure):
         prevPercent = 0
+        plt.ion()
+        plt.show()
+
         for i in range(self.hpoints):
             for j in range(self.vpoints):
-                if prevPercent < int(i * j / (self.hpoints * self.vpoints) * 100):
-                    prevPercent = int(i * j / (self.hpoints * self.vpoints) * 100)
-                    print(f"Progress: {prevPercent}%")
+
+                if prevPercent < int(i * j / (self.hpoints * self.vpoints) * 200):
+                    prevPercent = int(i * j / (self.hpoints * self.vpoints) * 200)
+                    print(f"Progress: {prevPercent/2}%")
+                    fig.axes[0].clear()
+                    self.ShowMaterialPlot(fig)
+                    fig.canvas.draw()
+                    fig.canvas.flush_events()
+
                 if material.isIntersect(self.array[i][j], cowl, (self.width, self.height)):
-                    self.array[i][j].material = DomainMaterial.COWL
+                    self.array[i,j].material = DomainMaterial.COWL
                 if material.isIntersect(self.array[i][j], coolant, (self.width, self.height)):
-                    self.array[i][j].material = DomainMaterial.COOLANT
+                    self.array[i,j].material = DomainMaterial.COOLANT
                 if material.isIntersect(self.array[i][j], chamber, (self.width, self.height)):
-                    self.array[i][j].material = DomainMaterial.CHAMBER
+                    self.array[i,j].material = DomainMaterial.CHAMBER
                 if material.isIntersect(self.array[i][j], plug, (self.width, self.height)):
-                    self.array[i][j].material = DomainMaterial.PLUG
+                    self.array[i,j].material = DomainMaterial.PLUG
+
+
+                
                 
     def ShowMaterialPlot(self, fig: plt.Figure):
         xarr = np.array([[point.x for point in row] for row in self.array])
@@ -62,12 +74,13 @@ class Domain:
         matarr = np.array([[point.material.value for point in row] for row in self.array])
 
         ax = fig.axes[0]
-        ax.contourf(xarr, rarr, matarr, 1)
+        contf = ax.contourf(xarr, rarr, matarr, 1)
         xcells = np.linspace(self.x0 - self.xstep/2, self.x0 + self.width + self.xstep/2, self.hpoints+1)
         rcells = np.linspace(self.r0 + self.rstep/2, self.r0 - self.height - self.rstep/2, self.vpoints+1)
         xl, rl = np.meshgrid(xcells, rcells)
-        ax.plot(xl, rl, 'k')
-        ax.plot(np.transpose(xl), np.transpose(rl), 'k')
+        ax.plot(xl, rl, 'k', linewidth=0.25)
+        ax.plot(np.transpose(xl), np.transpose(rl), 'k', linewidth=0.25)
+        return contf
                 
 
 
