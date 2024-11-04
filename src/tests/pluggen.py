966ec8681ca433f89f3cfec5c02955f17d32b5a5
David Anderson <anderd84@my.erau.edu>
2024-11-03 19:41:58 -0700

    plug sizes based on mdot

--- src/tests/pluggen.py
+++ src/tests/pluggen.py
@@ -20,34 +20,35 @@ from General.units import Q_, unitReg
 # ic(exhaustt.getChokedArea(Q_(8, ureg.pound/ureg.second)).to(ureg.inch**2))
 
 
-Re = Q_(3, unitReg.inch)
+Re = Q_(3.2, unitReg.inch)
 exhaust = DESIGN.exhaustGas
+ic(exhaust.stagTemp.to(unitReg.degR))
+ic(exhaust.stagPress.to(unitReg.psi))
+ic(exhaust.Rgas.to(unitReg.joule/unitReg.kg/unitReg.kelvin))
+ic(DESIGN.totalmdot.to(unitReg.kg/unitReg.second))
 
 cont, field, outputData = plug.CreateRaoContour(exhaust, DESIGN.chamberPressure, DESIGN.designAmbientPressure, DESIGN.basePressure, Re, DESIGN.lengthMax)
 Rt = outputData["radiusThroat"]
 Tt = outputData["thetaThroat"]
+Re = outputData["radiusLip"]
 ic(outputData["areaRatio"])
 ic(np.rad2deg(Tt))
+ic(Re)
 phi = np.pi/2 + Tt
 Astar = np.pi/np.sin(phi) * (Re**2 - Rt**2)
 ic(Astar)
 ic(DESIGN.chokeArea)
 
-c = 8/DESIGN.chokeArea.magnitude
-
-ic(Astar*c)
-
 Cf = outputData["Cf"]
 thrust = Astar * DESIGN.chamberPressure * Cf
 ic(thrust)
 ic(Cf)
-ic(outputData["thetaLip"])
 
 fig = plots.CreateNonDimPlot()
 # plots.PlotContour(fig, cont, Rt, Tt, Re)
 # plots.PlotField(fig, field, Re)
-plugC = plug.GenerateDimPlug(cont, Rt, Tt, Re, Q_(5, unitReg.inch), Q_(1.5, unitReg.inch))
-cowlC = plug.GenerateDimCowl(Rt, Tt, Re, Q_(8, unitReg.inch), DESIGN.maxRadius, DESIGN.wallThickness, Q_(0.025, unitReg.inch))
+plugC, straightLength = plug.GenerateDimPlug(cont, Rt, Tt, Re, Q_(5, unitReg.inch), Q_(1.5, unitReg.inch))
+cowlC = plug.GenerateDimCowl(Rt, Tt, Re, straightLength, DESIGN.chamberInternalRadius, DESIGN.wallThickness, Q_(0.025, unitReg.inch))
 plots.PlotPlug(fig, plugC)
 plots.PlotPlug(fig, cowlC)
 # mat, stream = analysis.CalculateComplexField(cont, 14/300, 15/300, exhaust, 1, Tt, Re.magnitude, 25, 0, 3)
