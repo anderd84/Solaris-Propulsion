622f0285e2177d064a25666532871c5938009482
Garth <nandakg@my.erau.edu>
2024-11-07 00:12:40 -0700

    Made the things more accurate so that th3e bottom and upper node actual use the cylindrical model

--- src/Cooling/analysis_Cooling.py
+++ src/Cooling/analysis_Cooling.py
@@ -6,6 +6,7 @@ from Nozzle import plug
 from General.units import Q_, unitReg
 from Cooling import cooling2d as cooling_func
 from Cooling.material import DomainMaterial 
+import numpy as np
 
 
 Re = Q_(3.2, unitReg.inch)
@@ -34,63 +35,83 @@ coolmesh: domain.DomainMC = domain.DomainMC.LoadFile("coolmesh2.msh")
 def leftnodeborder(coolmesh,i,j):
     #* Left Node
     if i==1:
-        left_node_wo_T_L = Q_(0, unitReg.degR)
-        deltaT_L = left_node_wo_T_L * coolmesh.array[i-1,j].temperature
+        left_node_wo_T = Q_(0, unitReg.degR)
+        deltaT_L = left_node_wo_T * coolmesh.array[i,j].temperature
     else:
         match coolmesh.array[i-1,j].material:
             case DomainMaterial.COWL:
-                left_node_wo_T_L = cooling_func.conduction(coolmesh.array[i-1,j].temperature)
-                deltaT_L = left_node_wo_T_L * coolmesh.array[i-1,j].temperature
+                left_node_wo_T = cooling_func.conduction(coolmesh.array[i-1,j].temperature)
+                deltaT_L = left_node_wo_T * coolmesh.array[i-1,j].temperature
             case DomainMaterial.CHAMBER:
-                left_node_wo_T_L = cooling_func.conduction(coolmesh.array[i-1,j].temperature,coolmesh.array[i-1,j].velocity)
-                deltaT_L = left_node_wo_T_L * coolmesh.array[i-1,j].temperature
-    return left_node_wo_T_L, deltaT_L
+                conduct = cooling_func.conduction(coolmesh.array[i-1,j].temperature)
+                convect = cooling_func.combustion_convection(coolmesh.array[i-1,j].temperature,coolmesh.array[i-1,j].velocity)
+                
+                left_node_wo_T = 1/(coolmesh.xstep/(conduct * coolmesh.rstep) + 1/(convect * coolmesh.rstep))
+                deltaT_L = left_node_wo_T * coolmesh.array[i-1,j].temperature
+    return left_node_wo_T, deltaT_L
 
 def uppernodeborder(coolmesh,i,j):
-    #* Upper Node
+    #* Upper Node 
     if j==1:
         left_node_wo_T_U = Q_(0, unitReg.degR)
-        deltaT_U = left_node_wo_T_U * coolmesh.array[i-1,j].temperature
+        deltaT_U = left_node_wo_T_U * coolmesh.array[i,j].temperature
     else:
-        match coolmesh.array[i,j-1].material:
+        match coolmesh.array[i, j + 1].material:
             case DomainMaterial.COWL:
-                left_node_wo_T_U = cooling_func.conduction(coolmesh.array[i,j-1].temperature)
-                deltaT_U = left_node_wo_T_U * coolmesh.array[i,j-1].temperature
+                upper_node_wo_T = cooling_func.conduction(coolmesh.array[i, j + 1].temperature)
+                deltaT_U = upper_node_wo_T * coolmesh.array[i, j + 1].temperature
             case DomainMaterial.CHAMBER:
-                left_node_wo_T_U = cooling_func.conduction(coolmesh.array[i,j-1].temperature,coolmesh.array[i,j-1].velocity)
-                deltaT_U = left_node_wo_T_U * coolmesh.array[i,j-1].temperature
+                conduct = cooling_func.conduction(coolmesh.array[i, j + 1].temperature)
+                
+                # Cylindrical conduction calculation using updated r_i and r_o
+                r_i = coolmesh.array[i, j].r  # Inner radius at [i, j]
+                r_o = coolmesh.array[i, j + 1].r  # Outer radius at [i, j+1]
+                l = coolmesh.xstep  # Axial length
 
-    return left_node_wo_T_U, deltaT_U
+                # Apply the cylindrical conduction formula
+                upper_node_wo_T = np.log(r_o / r_i) / (2 * np.pi * conduct * l)
+                deltaT_U = upper_node_wo_T * coolmesh.array[i, j + 1].temperature
+    return upper_node_wo_T, deltaT_U
 
 def bottomnodeborder(coolmesh,i,j):
     #* Bottom Node
     if coolmesh.array[i,j] == coolmesh.array[-1,j]:
         left_node_wo_T_B = Q_(0, unitReg.degR)
-        deltaT_B = left_node_wo_T_B * coolmesh.array[i+1,j].temperature
+        deltaT_B = left_node_wo_T_B * coolmesh.array[i,j].temperature
     else:
-        match coolmesh.array[i+1,j].material:
+        match coolmesh.array[i, j - 1].material:
             case DomainMaterial.COWL:
-                left_node_wo_T_B = cooling_func.conduction(coolmesh.array[i+1,j].temperature)
-                deltaT_B = left_node_wo_T_B * coolmesh.array[i+1,j].temperature
+                bottom_node_wo_T = cooling_func.conduction(coolmesh.array[i, j - 1].temperature)
+                deltaT_B = bottom_node_wo_T * coolmesh.array[i, j - 1].temperature
             case DomainMaterial.CHAMBER:
-                left_node_wo_T_B = cooling_func.conduction(coolmesh.array[i+1,j].temperature,coolmesh.array[i+1,j].velocity)ity)
-                deltaT_B = left_node_wo_T_B * coolmesh.array[i+1,j].temperature
-    return left_node_wo_T_B, deltaT_B
+                conduct = cooling_func.conduction(coolmesh.array[i, j - 1].temperature)
+                
+                # Define inner and outer radii for cylindrical conduction
+                r_i = coolmesh.array[i, j - 1].r  # Inner radius at [i, j-1]
+                r_o = coolmesh.array[i, j].r      # Outer radius at [i, j]
+                l = coolmesh.xstep                # Axial length
+
+                # Cylindrical conduction calculation
+                bottom_node_wo_T = np.log(r_o / r_i) / (2 * np.pi * conduct * l)
+                deltaT_B = bottom_node_wo_T * coolmesh.array[i, j - 1].temperature
+    return bottom_node_wo_T, deltaT_B
 
 def rightnodeborder(coolmesh,i,j):
     #* Right Node
     if coolmesh.array[i,j] == coolmesh.array[i,-1]:
-        left_node_wo_T_R = Q_(0, unitReg.degR)
-        deltaT_R = left_node_wo_T_R * coolmesh.array[i,j+1].temperature
+        right_node_wo_T = Q_(0, unitReg.degR)
+        deltaT_R = right_node_wo_T * coolmesh.array[i+1,j].temperature
     else:
-        match coolmesh.array[i,j+1].material:
+        match coolmesh.array[i+1,j].material:
             case DomainMaterial.COWL:
-                left_node_wo_T_R = cooling_func.conduction(coolmesh.array[i,j+1].temperature)
-                deltaT_R = left_node_wo_T_R * coolmesh.array[i,j+1].temperature
+                right_node_wo_T = cooling_func.conduction(coolmesh.array[i+1,j].temperature)
+                deltaT_R = right_node_wo_T * coolmesh.array[i+1,j].temperature
             case DomainMaterial.CHAMBER:
-                left_node_wo_T_R = cooling_func.conduction(coolmesh.array[i,j+1].temperature,coolmesh.array[i,j+1].velocity)
-                deltaT_R = left_node_wo_T_R * coolmesh.array[i,j+1].temperature
-    return left_node_wo_T_R, deltaT_R
+                conduct = cooling_func.conduction(coolmesh.array[i+1,j].temperature)
+                convect = cooling_func.combustion_convection(coolmesh.array[i+1,j].temperature,coolmesh.array[i+1,j].velocity)
+                right_node_wo_T = 1/(coolmesh.xstep/(conduct * coolmesh.rstep) + 1/(convect * coolmesh.rstep))
+                deltaT_R = right_node_wo_T * coolmesh.array[i+1,j].temperature    
+    return right_node_wo_T, deltaT_R
 
 
 def calculate_nozzle_area(A_star, M, gamma):
@@ -98,18 +119,26 @@ def calculate_nozzle_area(A_star, M, gamma):
     A = A_star * ((gamma + 1) / 2) ** (- (gamma + 1) / (2 * (gamma - 1))) * (1 + (gamma - 1) / 2 * M**2) ** ((gamma + 1) / (2 * (gamma - 1))) * (1 / M)
     return A
 
+
+
+
 for i in range(coolmesh.vpoints):
     for j in range(coolmesh.hpoints):
         #*Finding all options for barrier
         if coolmesh.array[i,j].border:
-            left_node_wo_T_L, deltaT_L = leftnodeborder(coolmesh,i,j)
-            left_node_wo_T_U, deltaT_U = uppernodeborder(coolmesh,i,j)
-            left_node_wo_T_B, deltaT_B = bottomnodeborder(coolmesh,i,j)
-            left_node_wo_T_R, deltaT_R = rightnodeborder(coolmesh,i,j)
-            #* Finally calculate Temperature for Node
-            Num = (deltaT_L + deltaT_U + deltaT_B + deltaT_R)
-            Denom = (left_node_wo_T_L + left_node_wo_T_U + left_node_wo_T_B + left_node_wo_T_R)
-            coolmesh.array[i,j].temperature = (Num/Denom).to(unitReg.degR)
+            match coolmesh.array[i,j].material:
+                case DomainMaterial.COWL:
+                    left_node_wo_T, deltaT_L = leftnodeborder(coolmesh,i,j)
+                    upper_node_wo_T, deltaT_U = uppernodeborder(coolmesh,i,j)
+                    bottom_node_wo_T, deltaT_B = bottomnodeborder(coolmesh,i,j)
+                    right_node_wo_T, deltaT_R = rightnodeborder(coolmesh,i,j)
+                    #* Finally calculate Temperature for Node
+                    Num = (deltaT_L + deltaT_U + deltaT_B + deltaT_R)
+                    Denom = (left_node_wo_T + upper_node_wo_T + bottom_node_wo_T + right_node_wo_T)
+                    coolmesh.array[i,j].temperature = (Num/Denom).to(unitReg.degR)
+                case DomainMaterial.CHAMBER:
+                    #DO nothing
+                    
 
 
             
