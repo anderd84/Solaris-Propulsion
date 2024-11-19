4ce058a97c94268b950c19727276bd5b6461283d
anderd84 <anderd84@my.erau.edu>
2024-11-18 22:41:23 -0700

    convergence plot!!!

--- src/Cooling/analysis.py
+++ src/Cooling/analysis.py
@@ -14,7 +14,7 @@ from Cooling import material
 from Cooling import analysisCoolingRef
 
 def AnalyzeMC(domain: domain.DomainMMAP, fig, plugC, cowlC, MAX_CORES: int = mp.cpu_count() - 1, tol: float = 1e-2):
-    # plt.ion()
+    plt.ion()
     # plt.show()
 
     # for col in range(domain.hpoints):
@@ -34,12 +34,22 @@ def AnalyzeMC(domain: domain.DomainMMAP, fig, plugC, cowlC, MAX_CORES: int = mp.
             #     fig.canvas.flush_events()
             #     plt.waitforbuttonpress()
             #     input("Press Enter to continue...")
+    diffArr = []
+    convergePlot, ax = plt.subplots()
+    ax.set_title("Convergence")
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Max % Difference")
+    ax.grid(True)
 
+    convergeP, = ax.plot([1], [1], 'r-')
 
     with joblib.Parallel(n_jobs=MAX_CORES, return_as='generator') as parallel:
         diff = tol + 1
         numRows = domain.vpoints
-        for i in range(100):
+        i = 0
+        while diff > tol:
+            i += 1
+            diff = 0
             with alive_bar(domain.vpoints, title=f"Analyzing iteration {i}") as bar:
                 outputs = parallel(
                     joblib.delayed(CalcRow)(domain, row) for row in range(numRows)
@@ -47,9 +57,20 @@ def AnalyzeMC(domain: domain.DomainMMAP, fig, plugC, cowlC, MAX_CORES: int = mp.
 
                 for output in outputs:
                     for row, col, tp in output:
+                        diff = max(diff, abs(domain.temperature[row, col].magnitude - tp[0].to(domain.units["temperature"]).magnitude)/domain.temperature[row, col].magnitude)
                         domain.setMEM(row, col, 'temperature', tp[0])
                         domain.setMEM(row, col, 'pressure', tp[1])
                     bar()
+
+            print(f"Max diff: {diff*100}%")
+            diffArr.append(diff*100)
+
+            del convergeP
+            convergeP = ax.plot(range(i), diffArr, 'r-')
+            ax.autoscale(axis='y')
+            
+            convergePlot.canvas.draw()
+            convergePlot.canvas.flush_events()
 
             if i % 10 == 0:
                 print("saving progress")
